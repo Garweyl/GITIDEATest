6234b2fc2de0339a2cb47efd8f887bd7c65974f7
tom_glb <goodmorning_glb@iii.com>
2020-12-29 15:15:21 +0800

    测试测试

---
+++ java1/src/Person.java
@@ -0,0 +1,16 @@
+/**
+ * @author Garweyl
+ * @create 2020-12-29-下午 03:02
+ */
+public class Person {
+
+    private Integer ade;
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("nihao nihao");
+    }
+
+
+
+}
